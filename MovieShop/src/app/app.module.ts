7aac031dfb4b612c31d5be1ca89ec524245d76af
Ryan <tornaldo07.rg@gmail.com>
2020-04-17 17:10:25 +0200

    fixes

--- MovieShop/src/app/app.module.ts
+++ MovieShop/src/app/app.module.ts
@@ -37,7 +37,7 @@ import { environment } from '../environments/environment';
     AppRoutingModule,
     FormsModule,  
     ReactiveFormsModule, 
-    ServiceWorkerModule.register('/ngsw-worker.js', { enabled:  environment.production })
+    ServiceWorkerModule.register('/Movie-App/ngsw-worker.js', { enabled:  environment.production })
   
   ],
   providers: [],
