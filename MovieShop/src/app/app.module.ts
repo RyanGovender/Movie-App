934c06accd5139ae01f795e3368cf990b4fe3aea
Ryan <tornaldo07.rg@gmail.com>
2020-04-17 16:45:38 +0200

    fixes

--- MovieShop/src/app/app.module.ts
+++ MovieShop/src/app/app.module.ts
@@ -14,6 +14,8 @@ import { RouterModule } from '@angular/router';
 import { MovieSearchComponent } from './movie-search/movie-search.component';
 import { HeroImageComponent } from './hero-image/hero-image.component';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { ServiceWorkerModule } from  '@angular/service-worker/'; 
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -34,7 +36,9 @@ import {FormsModule, ReactiveFormsModule} from '@angular/forms';
       // ),
     AppRoutingModule,
     FormsModule,  
-    ReactiveFormsModule,
+    ReactiveFormsModule, 
+    ServiceWorkerModule.register('/ngsw-worker.js', { enabled:  environment.production })
+  
   ],
   providers: [],
   bootstrap: [AppComponent]
--- MovieShop/src/main.ts
+++ MovieShop/src/main.ts
@@ -4,9 +4,16 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+platformBrowserDynamic().bootstrapModule(AppModule).then(() => {
+  if ('serviceWorker' in navigator && environment.production) {
+    navigator.serviceWorker.register('/ngsw-worker.js');
+  }
+}).catch(err => console.log(err));
+
 if (environment.production) {
   enableProdMode();
 }
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
+
